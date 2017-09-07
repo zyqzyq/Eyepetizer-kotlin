f15cf1060134195d62bf6fcfd47475f74815df84
zyqzyq <497533265@qq.com>
2017-09-07 09:07:38 +0800

    增加播放页面

---
+++ app/src/main/java/com/zyqzyq/eyepetizer/ui/activities/PlayActivity.kt
@@ -0,0 +1,13 @@
+package com.zyqzyq.eyepetizer.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.zyqzyq.eyepetizer.R
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
